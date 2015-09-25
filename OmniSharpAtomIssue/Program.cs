fc974a78a484310b6a5b196e1a9909361d12f0fd
Andrew Fontaine <AFontiane@phoenix-interactive.com>
2015-09-25 12:25:25 -0400

    Initial Commit

---
+++ OmniSharpAtomIssue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmniSharpAtomIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> bigList = new List<string>();
+        }
+    }
+}
